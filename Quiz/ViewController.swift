95105670b128c4f344259ddf13f72d7617cbd11c
d182_sofia_d <d182_sofia_d@macbook-11.local>
2018-02-16 19:47:54 -0600

    Initial Commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by d182_sofia_d on 16/02/18.
+//  Copyright © 2018 d182_sofia_d. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
